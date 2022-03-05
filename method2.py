9fe463bca8ffad1ea78f329b95ed72d7bebdd589
Seamus Mullan <24carrotgaming@gmail.com>
2022-03-05 12:47:37 +0000

    Fixed Noise, Gets Slope with linregress

--- method2.py
+++ method2.py
@@ -2,8 +2,9 @@
 import matplotlib.pyplot as plt
 import numpy
 import random
-
-from pytest import yield_fixture
+from scipy.stats import linregress
+import pprint
+# from pytest import yield_fixture
 
 ## Properties of normal Portland Cement Concrete
 
@@ -30,8 +31,6 @@ Shear_strength = 11.5
 
 
 
-
-
 """
 Assume that the strength changes with the amount of water to a certain point, and then drops off
 Also assume that the mix is useless below a certain threshold (eg: >20% water)
@@ -42,26 +41,33 @@ Graph the results and highlight the strongest mix
 
 
 
-## WATER PERCENTAGE CALCULATION
-
-def calcWaterPercent():
-    pass
-
-
 
 ## GRAPH PLOTTING
 
-def plotgraph(waterPercent):
-    straight_line = [i for i in range(0,100)]
+def plotgraph(waterPercent, x):
+    prediction_line = [i for i in range(0,100)]
+    # prediction_line = [0, 1, 2, 3, 4, 5, 6, 6.5, 7, 7.5, 7.75, 8.25, 8.5, 8, 7.8, 7.2, 6.3, 6, 5.8, 5.4, 5.1, 4.3, 3.4, 2.3, 1.4, 1, 0.8, 0.3]
+    
 
     data = []
     for i in range(0, 1000):
         data.append(i + random.randint(0,100))
     data = numpy.array(data)
-    data = numpy.convolve(data, straight_line, mode='same')
+    data = numpy.convolve(data, prediction_line, mode='same')
 
-    plt.plot(data)
-    plt.show()
+    data = [i for i in data * (waterPercent/100)]
+    ## AFFECT BASED ON WATER %
+
+    # print(data)
+
+    # slope, intercept, r, p, se = linregress(data, prediction_line)
+
+    # pprint.pprint(slope)
+
+
+
+    # plt.plot(data)
+    # plt.show()
 
 
     ## YIELD POINTS
@@ -86,9 +92,23 @@ def plotgraph(waterPercent):
 
 
 
-
-
     # Plot the stress strain graph and highlight the highest and lowest yield points
     plt.plot(data)
-    plt.axhline(y=maxPoint, color='r', linestyle='dotted') # Strongest mix
-    plt.show()
+    plt.axhline(y=maxPoint, color='b', linestyle='dotted') # Strongest mix
+    plt.savefig(f'graph{x}.png')
+
+
+
+
+def main():
+
+    ## WATER PERCENTAGE
+    waterPercent = [i for i in range(0,100)]
+
+    ## STRESS-STRAIN GRAPH
+    for i in waterPercent:
+        plotgraph(i, i)
+
+
+if __name__ == '__main__':
+    main()
